2eded171994f8df272cdd6832e5f6221296d8913
ShautPh <sauth.phouek168@gmail.com>
2021-12-17 21:35:52 +0700

    add

--- light.py
+++ light.py
@@ -15,59 +15,58 @@ carR = PhotoImage(file="./img/carR.png")
 carU = PhotoImage(file="./img/carU.png")
 carD = PhotoImage(file="./img/carD.png")
 
-# The Roads
-canvas.create_text(400,60,text="Traffic Light Loading...", fill="black", font=("Purisa",20,font.BOLD))
-# canvas.create_rectangle(300,100,500,700, fill="gray") # Road
+#Graphic street
 canvas.create_image(400, 400, image=street)
-
-# canvas.create_rectangle(10,300,790,500, fill="gray")
 canvas.create_image(400, 400, image=street2)
 
+
+# the first root
 canvas.create_rectangle(200,100,290,290, fill="black")
 y = 110 
 for i in range(3):
     canvas.create_oval(220,y,270, y+50, fill="gray")
     y += 60
+def light_green_1():
+    canvas.delete("light1")
+    canvas.create_oval(220,110,270, 160, fill="green", tags="light1")
+    canvas.after(6000,light_yellow_1)
+    drivingD()
+    drivingU()
+def light_yellow_1():
+    canvas.delete("light1")
+    canvas.create_oval(220,170,270, 220, fill="yellow", tags="light1")
+    canvas.after(3000,light_red_1)
+def light_red_1():
+    canvas.delete("light1")
+    canvas.create_oval(220,230,270, 280, fill="red", tags="light1")
+    canvas.after(6000,light_green_1)
+canvas.after(600,light_red_1)
+
+
+# # the second root
 canvas.create_rectangle(510,100,600,290, fill="black")
-
-
-# the first root
-def green1():
-    canvas.delete("light")
-    canvas.create_oval(220,110,270, 160, fill="green", tags="light")
-    canvas.after(5000,red1)
-def yellow1():
-    canvas.delete("light")
-    canvas.create_oval(220,170,270, 220, fill="yellow", tags="light")
-    canvas.after(1000,green1)
-def red1():
-    canvas.delete("light")
-    canvas.create_oval(220,230,270, 280, fill="red", tags="light")
-    canvas.after(5000,yellow1)
-canvas.after(100,red1)
-
-
-# the second root
 y = 110
 for i in range(3):
     canvas.create_oval(530,y,580, y+50, fill="gray")
     y += 60
 
-def green2():
-    canvas.delete("light")
-    canvas.create_oval(530,110,580, 160, fill="green", tags="light")
-    canvas.after(6000,red2)
+def light_green_2():
+    canvas.delete("light2")
+    canvas.create_oval(530,110,580, 160, fill="green", tags="light2")
+    canvas.after(6000,light_yellow_2)
+    drivingR()
+    drivingL()
 
-def yellow2():
-    canvas.delete("light")
-    canvas.create_oval(530,170,580, 220, fill="yellow", tags="light")
-    canvas.after(2000,green2)
+def light_yellow_2():
+    canvas.delete("light2")
+    canvas.create_oval(530,170,580, 220, fill="yellow", tags="light2")
+    canvas.after(3000,light_red_2)
 
-def red2():
-    canvas.delete("light")
-    canvas.create_oval(530,230,580, 280, fill="red", tags="light")
-    canvas.after(6000,yellow2)
-canvas.after(200,red2)
+def light_red_2():
+    canvas.delete("light2")
+    canvas.create_oval(530,230,580, 280, fill="red", tags="light2")
+    canvas.after(6000,light_green_2)
+canvas.after(600,light_green_2)
 
 
 # the third root
@@ -76,35 +75,52 @@ y = 520
 for i in range(3):
     canvas.create_oval(220,y,270, y+50, fill="gray")
     y += 60
-def green3():
-    canvas.delete("light")
-    canvas.create_oval(220,110,270, 160, fill="green", tags="light")
-    canvas.after(5000,red3)
-def yellow3():
-    canvas.delete("light")
-    canvas.create_oval(220,170,270, 220, fill="yellow", tags="light")
-    canvas.after(1000,green3)
-def red3():
-    canvas.delete("light")
-    canvas.create_oval(220,230,270, 280, fill="red", tags="light")
-    canvas.after(5000,yellow3)
-canvas.after(100,red3)
+def light_red_3():
+    canvas.delete("light3")
+    canvas.create_oval(220,520,270, 570, fill="red", tags="light3")
+    canvas.after(6000,light_green_3)
+def light_yellow_3():
+    canvas.delete("light3")
+    canvas.create_oval(220,580,270, 630, fill="yellow", tags="light3")
+    canvas.after(3000,light_red_3)
+def light_green_3():
+    drivingR()
+    drivingL()
+    canvas.delete("light3")
+    canvas.create_oval(220,640,270, 690, fill="green", tags="light3")
+    canvas.after(6000,light_yellow_3)
+canvas.after(600,light_green_3)
 
 
-
+# # the fourth root
 canvas.create_rectangle(510,510,600,700, fill="black")
 y = 520
 for i in range(3):
     canvas.create_oval(530,y,580, y+50, fill="gray")
     y += 60
+def red4():
+    canvas.delete("light4")
+    canvas.create_oval(530,520,580, 570, fill="red", tags="light4")
+    canvas.after(6000,green4)
+def yellow4():
+    canvas.delete("light4")
+    canvas.create_oval(530,580,580, 630, fill="yellow", tags="light4")
+    canvas.after(3000,red4)
+
+def green4():
+    canvas.delete("light4")
+    canvas.create_oval(530,640,580, 690, fill="green", tags="light4")
+    canvas.after(6000,yellow4)
+    drivingU()
+    drivingD()
+canvas.after(600,red4)
 
 myCar1 = canvas.create_image(0, 450, image=car)
 myCar2 = canvas.create_image(750, 350, image=carR)
 myCar3 = canvas.create_image(450, 640, image=carU)
 myCar4 = canvas.create_image(350, 40, image=carD)
 
-# Driving
-# left 
+# Driving left 
 def drivingL():
     global myCar1
     pos = canvas.coords(myCar1)
@@ -147,10 +163,7 @@ def drivingD():
     else:
         myCar4 = canvas.create_image(350, 40, image=carD)
     canvas.after(150, drivingD)
-drivingR()
-drivingL()
-drivingU()
-drivingD()
+
 
 canvas.pack(expand=True,fill='both')
 frame.pack(expand=True,fill='both')
